e57c1f0c449332cedfd3efbd9814597abb31dc25
Quanuanc <quanuanc@outlook.com>
2024-01-18 21:50:53 +0800

    refactor: update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,6 @@ local keys = {
 	{ key = "Enter", mods = "META", action = wezterm.action.DisableDefaultAssignment },
 
 	-- tmux like key binding
-	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 	{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 	{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 	{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
@@ -24,14 +22,8 @@ local keys = {
 	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
 	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
 	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
-	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
-	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
-	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
-	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
-	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
-	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "m", mods = "LEADER", action = wezterm.action({ PaneSelect = { mode = "SwapWithActive" } }) },
 }
 
 local config = {}
@@ -47,7 +39,7 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.initial_cols = 155
 config.initial_rows = 44
 config.font_size = 13
-config.leader = { key = ";", mods = "CTRL" }
+config.leader = { key = " ", mods = "CTRL" }
 config.keys = keys
 config.front_end = "WebGpu"
 
