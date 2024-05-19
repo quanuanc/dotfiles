1649a0cb5c5eba0ec9636ccec82fcbfc67e83bd7
Quanuanc <quanuanc@outlook.com>
2024-05-19 22:41:01 +0800

    feat(nvim): Add beancount-black

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,7 +8,15 @@ return {
         c = { "clang-format" },
         json = { "biome" },
         python = { "ruff_format" },
+        beancount = { "bean_black" },
         ["_"] = { { "prettierd", "prettier" } },
+      },
+      formatters = {
+        bean_black = {
+          command = "bean-black",
+          args = { "-s" },
+          stdin = true,
+        },
       },
     })
   end,
